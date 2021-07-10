7ebebb94f14d8974d3737340418b66f223ca5001
Derrick Bush <derrickbush1999@gmail.com>
2021-07-10 17:38:41 -0500

    Added ability to use with either a with statment our not. Using without a with statment allows you to use the API thoughout your code. To update your code simply add vmr.login() and replace the with statment with just vmr=voicemeeter.remote(kind)

--- README.md
+++ README.md
@@ -44,6 +44,35 @@ with voicemeeter.remote(kind) as vmr:
   # Resets all UI elements to a base profile
   vmr.reset()
 ```
+Or if you want to use the API outside of a with statment
+```python
+import voicemeeter
+
+# Can be 'basic', 'banana' or 'potato'
+kind = 'potato'
+
+# Ensure that Voicemeeter is launched
+voicemeeter.launch(kind)
+
+vmr = voicemeeter.remote(kind)
+vmr.login()
+# Set the mapping of the second input strip
+vmr.inputs[1].A4 = True
+print(f'Output A4 of Strip {vmr.inputs[1].label}: {vmr.inputs[1].A4}')
+
+# Set the gain slider of the leftmost output bus
+vmr.outputs[0].gain = -6.0
+
+# Also supports assignment through a dict
+vmr.apply({
+'in-5': dict(A1=True, B1=True, gain=-6.0),
+'out-2': dict(mute=True)
+})
+
+# Resets all UI elements to a base profile
+vmr.reset()
+vmr.logout()
+```
 
 ## Profiles
 Profiles through config files are supported.
--- voicemeeter/remote.py
+++ voicemeeter/remote.py
@@ -12,171 +12,189 @@ from .util import merge_dicts
 
 loggedIn = False
 
-class VMRemote(abc.ABC):
-  """ Wrapper around Voicemeeter Remote's C API. """
-  def __init__(self, delay=.015):
-    self.delay = delay
-    self.cache = {}
 
-  def _call(self, fn, *args, check=True, expected=(0,)):
-    """
+class VMRemote(abc.ABC):
+    """ Wrapper around Voicemeeter Remote's C API. """
+
+    def __init__(self, delay=.015):
+        self.delay = delay
+        self.cache = {}
+
+    def _call(self, fn, *args, check=True, expected=(0,)):
+        """
     Runs a C API function.
-    
+
     Raises an exception when check is True and the
     function's return value is not 0 (OK).
     """
-    fn_name = 'VBVMR_' + fn
-    retval = getattr(dll, fn_name)(*args)
-    if check and retval not in expected:
-      raise VMRDriverError(fn_name, retval)
-    time.sleep(self.delay)
-    return retval
+        fn_name = 'VBVMR_' + fn
+        retval = getattr(dll, fn_name)(*args)
+        if check and retval not in expected:
+            raise VMRDriverError(fn_name, retval)
+        time.sleep(self.delay)
+        return retval
 
-  def _login(self):
-    global loggedIn
-    if(loggedIn == False):
-      self._call('Login')
-      loggedIn = True
-  def _logout(self):
-    self._call('Logout')
-    
-  def logout(self):
-    self._logout()
+    def _login(self):
+        global loggedIn
+        if (loggedIn == False):
+            self._call('Login')
+            loggedIn = True
 
+    def _logout(self):
+        global loggedIn
+        if (loggedIn == True):
+            self._call('Logout')
+            loggedIn = False
 
-  @property
-  def type(self):
-    """ Returns the type of Voicemeeter installation (basic, banana, potato). """
-    buf = ct.c_long()
-    self._call('GetVoicemeeterType', ct.byref(buf))
-    val = buf.value
-    if val == 1:
-      return 'basic'
-    elif val == 2:
-      return 'banana'
-    elif val == 3:
-      return 'potato'
-    else:
-      raise VMRError(f'Unexpected Voicemeeter type: {val}')
+    def login(self):
+        global loggedIn
+        if (loggedIn == False):
+            self._call('Login')
+            loggedIn = True
 
-  @property
-  def version(self):
-    """ Returns Voicemeeter's version as a tuple (v1, v2, v3, v4) """
-    buf = ct.c_long()
-    self._call('GetVoicemeeterVersion', ct.byref(buf))
-    v1 = (buf.value & 0xFF000000) >> 24
-    v2 = (buf.value & 0x00FF0000) >> 16
-    v3 = (buf.value & 0x0000FF00) >> 8
-    v4 = (buf.value & 0x000000FF)
-    return (v1, v2, v3, v4)
+    def logout(self):
+        self._logout()
 
-  @property
-  def dirty(self):
-    """ True iff UI parameters have been updated. """
-    val = self._call('IsParametersDirty', expected=(0,1))
-    return (val == 1)
-  
-  def get(self, param, string=False):
-    """ Retrieves a parameter. """
-    param = param.encode('ascii')
-    if not self.dirty:
-      if param in self.cache:
-        pass
-        #return self.cache[param]
+    @property
+    def type(self):
+        """ Returns the type of Voicemeeter installation (basic, banana, potato). """
+        buf = ct.c_long()
+        self._call('GetVoicemeeterType', ct.byref(buf))
+        val = buf.value
+        if val == 1:
+            return 'basic'
+        elif val == 2:
+            return 'banana'
+        elif val == 3:
+            return 'potato'
+        else:
+            raise VMRError(f'Unexpected Voicemeeter type: {val}')
 
-    if string:
-      buf = (ct.c_wchar * 512)()
-      self._call('GetParameterStringW', param, ct.byref(buf))
-    else:
-      buf = ct.c_float()
-      self._call('GetParameterFloat', param, ct.byref(buf))
-    val = buf.value
-    self.cache[param] = val
-    return val
-  
-  def set(self, param, val):
-    """ Updates a parameter. """
-    param = param.encode('ascii')
-    if isinstance(val, str):
-      if len(val) >= 512:
-        raise VMRError('String is too long')
-      self._call('SetParameterStringW', param, ct.c_wchar_p(val))
-    else:
-      self._call('SetParameterFloat', param, ct.c_float(float(val)))
+    @property
+    def version(self):
+        """ Returns Voicemeeter's version as a tuple (v1, v2, v3, v4) """
+        buf = ct.c_long()
+        self._call('GetVoicemeeterVersion', ct.byref(buf))
+        v1 = (buf.value & 0xFF000000) >> 24
+        v2 = (buf.value & 0x00FF0000) >> 16
+        v3 = (buf.value & 0x0000FF00) >> 8
+        v4 = (buf.value & 0x000000FF)
+        return (v1, v2, v3, v4)
 
-  def show(self):
-    """ Shows Voicemeeter if it's hidden. """
-    self.set('Command.Show', 1)
-  def shutdown(self):
-    """ Closes Voicemeeter. """
-    self.set('Command.Shutdown', 1)
-  def restart(self):
-    """ Restarts Voicemeeter's audio engine. """
-    self.set('Command.Restart', 1)
+    @property
+    def dirty(self):
+        """ True iff UI parameters have been updated. """
+        val = self._call('IsParametersDirty', expected=(0, 1))
+        return (val == 1)
 
-  def apply(self, mapping):
-    """ Sets all parameters of a dict. """
-    for key, submapping in mapping.items():
-      strip, index = key.split('-')
-      index = int(index)
-      if strip in ('in', 'input'):
-        target = self.inputs[index]
-      elif strip in ('out', 'output'):
-        target = self.outputs[index]
-      else:
-        raise ValueError(strip)
-      target.apply(submapping)
-  
-  def apply_profile(self, name):
-    try:
-      profile = self.profiles[name]
-      if 'extends' in profile:
-        base = self.profiles[profile['extends']]
-        profile = merge_dicts(base, profile)
-        del profile['extends']
-      self.apply(profile)
-    except KeyError:
-      raise VMRError(f'Unknown profile: {self.kind.id}/{name}')
+    def get(self, param, string=False):
+        """ Retrieves a parameter. """
+        param = param.encode('ascii')
+        if not self.dirty:
+            if param in self.cache:
+                pass
+                # return self.cache[param]
 
-  def reset(self):
-    self.apply_profile('base')
+        if string:
+            buf = (ct.c_wchar * 512)()
+            self._call('GetParameterStringW', param, ct.byref(buf))
+        else:
+            buf = ct.c_float()
+            self._call('GetParameterFloat', param, ct.byref(buf))
+        val = buf.value
+        self.cache[param] = val
+        return val
 
-  def __enter__(self):
-    self._login()
-    return self
-  def __exit__(self, type, value, traceback):
-    pass
+    def set(self, param, val):
+        """ Updates a parameter. """
+        param = param.encode('ascii')
+        if isinstance(val, str):
+            if len(val) >= 512:
+                raise VMRError('String is too long')
+            self._call('SetParameterStringW', param, ct.c_wchar_p(val))
+        else:
+            self._call('SetParameterFloat', param, ct.c_float(float(val)))
+
+    def show(self):
+        """ Shows Voicemeeter if it's hidden. """
+        self.set('Command.Show', 1)
+
+    def shutdown(self):
+        """ Closes Voicemeeter. """
+        self.set('Command.Shutdown', 1)
+
+    def restart(self):
+        """ Restarts Voicemeeter's audio engine. """
+        self.set('Command.Restart', 1)
+
+    def apply(self, mapping):
+        """ Sets all parameters of a dict. """
+        for key, submapping in mapping.items():
+            strip, index = key.split('-')
+            index = int(index)
+            if strip in ('in', 'input'):
+                target = self.inputs[index]
+            elif strip in ('out', 'output'):
+                target = self.outputs[index]
+            else:
+                raise ValueError(strip)
+            target.apply(submapping)
+
+    def apply_profile(self, name):
+        try:
+            profile = self.profiles[name]
+            if 'extends' in profile:
+                base = self.profiles[profile['extends']]
+                profile = merge_dicts(base, profile)
+                del profile['extends']
+            self.apply(profile)
+        except KeyError:
+            raise VMRError(f'Unknown profile: {self.kind.id}/{name}')
+
+    def reset(self):
+        self.apply_profile('base')
+
+    def __enter__(self):
+        self._login()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.logout()
 
 
 def _make_remote(kind):
-  """
+    """
   Creates a new remote class and sets its number of inputs
   and outputs for a VM kind.
-  
+
   The returned class will subclass VMRemote.
   """
-  def init(self, *args, **kwargs):
-    VMRemote.__init__(self, *args, **kwargs)
-    self.kind = kind
-    self.num_A, self.num_B = kind.layout
-    self.inputs = tuple(InputStrip.make((i < self.num_A), self, i) for i in range(self.num_A+self.num_B))
-    self.outputs = tuple(OutputBus.make((i < self.num_B), self, i) for i in range(self.num_A+self.num_B))
-  def get_profiles(self):
-    return profiles.profiles[kind.id]
- 
-  return type(f'VMRemote{kind.name}', (VMRemote,), {
-    '__init__': init,
-    'profiles': property(get_profiles)
-  })
+
+    def init(self, *args, **kwargs):
+        VMRemote.__init__(self, *args, **kwargs)
+        self.kind = kind
+        self.num_A, self.num_B = kind.layout
+        self.inputs = tuple(InputStrip.make((i < self.num_A), self, i) for i in range(self.num_A + self.num_B))
+        self.outputs = tuple(OutputBus.make((i < self.num_B), self, i) for i in range(self.num_A + self.num_B))
+
+    def get_profiles(self):
+        return profiles.profiles[kind.id]
+
+    return type(f'VMRemote{kind.name}', (VMRemote,), {
+        '__init__': init,
+        'profiles': property(get_profiles)
+    })
+
 
 _remotes = {kind.id: _make_remote(kind) for kind in kinds.all}
 
+
 def connect(kind_id, delay=None):
-  if delay is None:
-    delay = .015
-  """ Connect to Voicemeeter and sets its strip layout. """
-  try:
-    cls = _remotes[kind_id]
-    return cls(delay=delay)
-  except KeyError as err:
-    raise VMRError(f'Invalid Voicemeeter kind: {kind_id}')
+    if delay is None:
+        delay = .015
+    """ Connect to Voicemeeter and sets its strip layout. """
+    try:
+        cls = _remotes[kind_id]
+        return cls(delay=delay)
+    except KeyError as err:
+        raise VMRError(f'Invalid Voicemeeter kind: {kind_id}')
