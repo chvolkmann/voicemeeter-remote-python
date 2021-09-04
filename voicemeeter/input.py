6c4c3eb9c143ca259c8e30b13b0b89a176017dd3
Derrick Bush <derrickbush1999@gmail.com>
2021-09-04 07:58:15 -0500

    Fixed limit not being settable.

--- voicemeeter/input.py
+++ voicemeeter/input.py
@@ -25,7 +25,7 @@ class InputStrip(VMElement):
   gain = float_prop('Gain', range=(-60,12))
   comp = float_prop('Comp', range=(0,10))
   gate = float_prop('Gate', range=(0,10))
-  gate = float_prop('Gate', range=(0,10))
+  limit = float_prop('Limit', range=(-40,10))
 
   label = str_prop('Label')
   device = str_prop('device.name')
